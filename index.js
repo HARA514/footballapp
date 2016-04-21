d63f2e6fc1956db5c90af2246f501187cbf63c7a
HARA514 <HARA5_14@uni.worc.ac.uk>
2016-04-21 18:01:46 +0100

    Updated code
    Updated code

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 // JavaScript Document
 
+var feedURL = "http://api.football-data.org/v1/soccerseasons/398/teams";
+
+$(document).on('pageshow', '#premierleague', function(event) {
+	
+	
+	<!-- Use an HTML GET request to obtain data from a Yahoo Pipe
+	<!-- The Yahoo pipe currently parses the BBC News RSS feed  -->
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.send();
+		
+		
+	<!-- parse the resulting JSON into Javascript Data Object -->
+	<!-- you can use a live parser to inspect the contents of the JSON
+	<!-- http://json.parser.online.fr/ -->
+	var teams= JSON.parse(xmlhttp.responseText);
+	
+	alert(teams);
+	
+	
+	<!-- Define Ractive binding -->
+	//var ractive = new Ractive({
+    //	el: 'container', <!-- where -->
+    //	template: '#myTemplate', <!-- how -->
+    //	data: { weather : weather.consolidated_weather } <!-- what - specify the list of news articles 'items' using dot notation-->
+//	});
+	
+});
+
+
+
+
+
+
+
+	
+function getData() {
 $.ajax({
   headers: { 'X-Auth-Token': 'e41abc632db14f7cabc6d28ae1efcb7e' },
-  url: 'http://api.football-data.org/v1/teams/66/players',
-
-
-
-var url = "http://api.football-data.org/v1/teams/66/players";
-    $.getJSON(url,function (data, textStatus) {
-           var players=data.results[0].name;
-          console.log(name);
-      });
-	
+  url: 'http://api.football-data.org/v1/fixtures?timeFrame=n1',
+  dataType: 'json',
+  type: 'GET',
+}).done(function(response) {
+  // do something with the response, e.g. isolate the id of a linked resource        
+  var regex = /.*?(\d+)$/; // the ? makes the first part non-greedy
+  var res = regex.exec(response.fixtures[0]._links.awayTeam.href);
+  var teamId = res[1];
+  console.log(teamId);
+  $('#teamID').html(teamId);
+});
+}
