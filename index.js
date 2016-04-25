007a2e04e8bc46fd621aef629441ad015320a6ab
HARA514 <HARA5_14@uni.worc.ac.uk>
2016-04-25 11:57:36 +0100

    123
    123

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // JavaScript Document
-
+var text = "";
+var i;
 var feedURL = "http://api.football-data.org/v1/soccerseasons/398/teams";
 
 var selectedTeamURL;
@@ -12,7 +13,13 @@ $(document).on('pageshow', '#premierleague', function(event) {
 		
 	var teams= JSON.parse(xmlhttp.responseText);
 
-	$('#teams').append(teams.teams[0].name);
+	$('#teams').append(teams.teams[1].name);
+	
+	for (i = 0; i < teams.teams[i].name.length; i++) { 
+    text += name[i];
+	
+	document.getElementById("test").innerHTML = text;
+}
 		
 	
 	
