62960a2862a2e1b7403b0034f2b826878d4befd5
HARA514 <HARA5_14@uni.worc.ac.uk>
2016-04-28 16:20:15 +0100

    API names
    API names

--- index.js
+++ index.js
@@ -315,11 +315,28 @@ var ManchesterUnited = [
 
 $(document).on("pagecreate","#manchesterUnited", onPageCreated);
 function onPageCreated() {
+
+	var feedUrl = "http://api.football-data.org/v1/teams/66/players";
+	
+		
+
+
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.send();
+		
+	var teamsObject= JSON.parse(xmlhttp.responseText);
+
+	var myTeam = teamsObject.players;
+	
+
+
 	ractive = new Ractive({
 	el: '#listManu',
 	
 	template: '#template',
 	
-	data: { listManu: ManchesterUnited}
+	//data: { listManu: ManchesterUnited}
+	data: { listManu: myTeam}
 });
 }
