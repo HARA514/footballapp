4a39d7531ff09e01c4063582ece699f064642e66
HARA514 <HARA5_14@uni.worc.ac.uk>
2016-04-28 18:03:44 +0100

    12
    12

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 $(document).on("pagecreate","#manchesterUnited", onPageCreated);
 function onPageCreated() {
 
-	var feedUrl = "http://api.football-data.org/v1/teams/66/players";
+	var manchesterUnitedFeed = "http://api.football-data.org/v1/teams/66/players";
 
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedUrl, false);
+	xmlhttp.open("GET", manchesterUnitedFeed, false);
 	xmlhttp.send();
 		
 	var teamsObject= JSON.parse(xmlhttp.responseText);
@@ -20,3 +19,26 @@ function onPageCreated() {
 	data: {listManu: myTeam}
 });
 }
+
+$(document).on("pagecreate","#manchesterCity", onPageCreated);
+function onPageCreated() {
+
+	var manchesterCityFeed = "http://api.football-data.org/v1/teams/65/players";
+
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", manchesterCityFeed, false);
+	xmlhttp.send();
+		
+	var teamsObject= JSON.parse(xmlhttp.responseText);
+
+	var myTeam = teamsObject.players;
+
+	ractive = new Ractive({
+	el: '#listManCity',
+	
+	template: '#template',
+	
+	data: {listManCity: myTeam}
+});
+}
+
